99874ae5e87bdf7ec2606fa9e84900804580ed7e
Rishabh Dubey <rishabh.dubey.cs21@ggits.net>
2023-08-25 15:31:43 +0530

    cloudinaryt added

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,19 +1,21 @@
 const express = require("express");
 const {
-
   authUser,
   allUsers,
 } = require("../controllers/userControllers");
 const asyncHandler = require('express-async-handler');
-const { protect } = require("../middleware/authMiddleware");
 const User = require("../models/userModel");
-const jwt = require("jsonwebtoken");
-const multer = require('multer');
+ // Import the Cloudinary SDK
 const router = express.Router();
 const path = require('path');
 const generateToken = require('../config/generateToken');
+const fs = require('fs');
+// Configure Cloudinary
+const cloudinary = require('../utils/cloudinary');
+
+// Configure multer for image upload
+const multer = require('multer');
 const storage = multer.diskStorage({
-  
   destination: function (req, file, cb) {
     cb(null, './uploads');
   },
@@ -21,8 +23,41 @@ const storage = multer.diskStorage({
     cb(null, file.fieldname + '-' + Date.now() + path.extname(file.originalname));
   },
 });
-
 const upload = multer({ storage: storage });
+
+// Route for uploading user images to Cloudinary
+router.post('/upload', upload.single('image'), asyncHandler(async (req, res) => {
+  const userId = req.query.userId;
+
+  // Find the user based on the user ID
+  const user = await User.findById(userId);
+
+  if (!user) {
+    res.status(404).json({ message: 'User not found' });
+    return;
+  }
+
+  try {
+    // Upload image to Cloudinary
+    const uploadResult = await cloudinary.uploader.upload(req.file.path, {
+      folder: 'user-profiles' // Specify the folder where you want to store images
+    });
+
+    // Update the user's pic field with the Cloudinary image URL
+    user.pic = uploadResult.secure_url;
+    await user.save();
+
+    // Delete the local image file after upload
+    fs.unlinkSync(req.file.path);
+
+    res.status(200).json({ message: 'Image uploaded successfully' });
+  } catch (error) {
+    console.error('Error uploading image to Cloudinary:', error);
+    res.status(500).json({ message: 'Image upload failed' });
+  }
+}));
+
+
 router.route("/").get(allUsers);
 // router.route("/signup").post(registerUser);
 router.post("/login", authUser);
@@ -58,27 +93,49 @@ router.post("/signup", asyncHandler(async (req, res) => {
 
 
 // Route for uploading user images
-router.post('/upload', upload.single('image'), asyncHandler(async (req, res) => {
-  const userId = req.query.userId; // Get the user ID from the query parameters if needed
+// router.post('/upload', upload.single('image'), asyncHandler(async (req, res) => {
+//   const userId = req.query.userId; // Get the user ID from the query parameters if needed
 
-  // Find the user based on the user ID
-  const user = await User.findById(userId);
-console.log("\n\n\nuser in upload route :", user)
+//   // Find the user based on the user ID
+//   const user = await User.findById(userId);
+// console.log("\n\n\nuser in upload route :", user)
 
-  if (!user) {
-    res.status(404).json({ message: 'User not found' });
-    return;
+//   if (!user) {
+//     res.status(404).json({ message: 'User not found' });
+//     return;
+//   }
+//   console.log("\n\n\nreq in upload route :", req)
+//   console.log("\n\n\nreq.file in upload route :", req.file)
+// console.log("\n\n\nreq.file.path in upload route :", req.file.path)
+//   // Update the user's pic field with the image path
+//   user.pic = req.file.path;
+//   await user.save();
+
+//   res.status(200).json({ message: 'Image uploaded successfully' });
+// })
+// );
+
+
+
+
+router.get('/:userId', async (req, res) => {
+  const userId = req.params.userId;
+
+  try {
+    const user = await User.findById(userId);
+
+    if (!user) {
+      return res.status(404).json({ message: 'User not found' });
+    }
+
+    return res.status(200).json({
+      username: user.username,
+      pic: user.pic // Send the image path
+    });
+  } catch (error) {
+    return res.status(500).json({ message: 'An error occurred' });
   }
-  console.log("\n\n\nreq in upload route :", req)
-  console.log("\n\n\nreq.file in upload route :", req.file)
-console.log("\n\n\nreq.file.path in upload route :", req.file.path)
-  // Update the user's pic field with the image path
-  user.pic = req.file.path;
-  await user.save();
-
-  res.status(200).json({ message: 'Image uploaded successfully' });
-})
-);
+});
 
 
 module.exports = router;
--- frontend/src/components/UserCard.tsx
+++ frontend/src/components/UserCard.tsx
@@ -1,24 +1,58 @@
 import React from 'react';
+import { useQuery } from 'react-query';
+import axios from 'axios';
 
 interface User {
-  _id: string;
-  name: string;
-  email: string;
-  pic: string;
+    _id: string;
+    name: string;
+    email: string;
+    pic: string;
 }
 
 interface UserCardProps {
-  user: User;
+    user: User;
 }
 
+const getUserDetails = async (userId: string) => {
+    const response = await axios.get(`/api/user/${userId}`);
+    return response.data;
+};
+
 const UserCard: React.FC<UserCardProps> = ({ user }) => {
-  return (
-    <div className="border p-4 rounded shadow">
-      <img src={user.pic} alt={user.name} className="w-full h-auto mb-2" />
-      <h3 className="text-xl font-semibold">{user.name}</h3>
-      <p className="text-gray-500">{user.email}</p>
-    </div>
-  );
+    const { data: userData, isLoading, isError } = useQuery(['user', user._id], () => getUserDetails(user._id));
+
+    if (isLoading) {
+        return <div>Loading...</div>;
+    }
+
+    if (isError || !userData) {
+        return <div>Error fetching user data.</div>;
+    }
+
+    const PF = "http://localhost:8000/";
+    const defaultImg =
+        "https://c4.wallpaperflare.com/wallpaper/72/624/1004/anime-one-piece-zoro-roronoa-wallpaper-preview.jpg";
+
+    const imageUrl = user.pic ? `${PF}${user.pic.replace(/\\/g, '/')}` : defaultImg;
+
+    const myObject = {
+        user: user.email,
+        imageUrl: imageUrl,
+        'user.pic': user.pic,
+        PF: PF,
+        'userPicReplaced': user.pic.replace(/\\/g, '/'),
+        defaultImg: defaultImg
+    };
+
+    console.log(myObject);
+
+    return (
+        <div className="border p-4 rounded shadow">
+            <img src={user.pic} alt={user.name} className="w-full h-auto mb-2" />
+            <h3 className="text-xl font-semibold">{user.name}</h3>
+            <p className="text-gray-500">{user.email}</p>
+        </div>
+    );
 };
 
 export default UserCard;
